692387e9888effed9ee7f46685a8ce98fa5cd949
atharvv01 <athzanwa@gmail.com>
2024-03-08 16:00:45 +0530

    created singup component and refined login page

--- 06_Frontend/Get_a_Room/src/app/app.routes.ts
+++ 06_Frontend/Get_a_Room/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent}
 ];
