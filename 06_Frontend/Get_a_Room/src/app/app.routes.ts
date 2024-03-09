7b0052364d86f75a7385a0326d02b110d791f2a9
pranay jain <pranayjain1382.com>
2024-03-09 17:49:20 +0530

    To pull the changes

--- 06_Frontend/Get_a_Room/src/app/app.routes.ts
+++ 06_Frontend/Get_a_Room/src/app/app.routes.ts
@@ -1,23 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
 import {PropertiesComponent} from './properties/properties.component'
 import { SignupComponent } from './signup/signup.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
-=======
-import { SignupComponent } from './signup/signup.component';
->>>>>>> eddb87a8acf0d09abf69d884af146b427827d51d
+
 
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   {path:'login',component:LoginComponent},
-<<<<<<< HEAD
   {path:'properties',component:PropertiesComponent},
   {path:'signup',component:SignupComponent},
   {path:'searchresult',component:SearchResultsComponent}
-=======
-  {path:'signup',component:SignupComponent}
->>>>>>> eddb87a8acf0d09abf69d884af146b427827d51d
+
 ];
