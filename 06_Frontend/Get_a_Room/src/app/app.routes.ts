1d436cc49efe7e97e0d7b64ce85a0ec246cf4a7e
Sagar <sagar.arora198@gmail.com>
2024-03-09 19:02:01 +0530

    wallet management-page

--- 06_Frontend/Get_a_Room/src/app/app.routes.ts
+++ 06_Frontend/Get_a_Room/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { PropertiesComponent } from './properties/properties.component';
 import { HotelDescriptionComponent } from './hotel-description/hotel-description.component';
+import { RoomdetailsComponent } from './roomdetails/roomdetails.component';
+import { WalletManagementComponent } from './wallet-management/wallet-management.component';
 
 
 export const routes: Routes = [
@@ -11,5 +13,7 @@ export const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
   {path:'properities',component:PropertiesComponent},
-  {path:'hotelDesc',component:HotelDescriptionComponent}
+  {path:'hotelDesc',component:HotelDescriptionComponent},
+  {path:'roomdetail',component:RoomdetailsComponent},
+  {path:'walletmanage',component:WalletManagementComponent}
 ];
