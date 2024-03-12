79dbbd4e058747d8897b64ac31503327f7618219
Sumit4482 <sbanwakde4482@gmail.com>
2024-03-12 14:54:30 +0530

    changes in the file structure

---
+++ 05_backend/database.js
@@ -0,0 +1,13 @@
+// database.js
+
+import mongoose from "mongoose";
+
+export function connectDatabase() {
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("🚀 Connected to Database Successfully 🚀");
+        })
+        .catch(error => {
+            console.error("Error connecting to database:", error);
+        });
+}
---
+++ 05_backend/swaggerSetup.js
@@ -0,0 +1,29 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+export function setupSwagger(app) {
+    // Swagger definition
+    const swaggerDefinition = {
+        openapi: '3.0.0',
+        info: {
+            title: 'Express API for JSONPlaceholder',
+            version: '1.0.0',
+        },
+        servers: [{
+            url: "http://localhost:3000/"
+        }]
+    };
+
+    // Options for the swagger docs
+    const options = {
+        swaggerDefinition,
+        // Paths to files containing OpenAPI definitions
+        apis: ['./routes/*.js'],
+    };
+
+    // Initialize swagger-jsdoc -> returns validated swagger spec in json format
+    const swaggerSpec = swaggerJSDoc(options);
+
+    // Serve swagger docs
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
