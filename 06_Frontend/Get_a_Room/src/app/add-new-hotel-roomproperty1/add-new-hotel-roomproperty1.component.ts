f6cc23907c2092aac8891ac262cc51764415881b
pranay jain <pranayjain1382.com>
2024-03-14 16:04:18 +0530

    adminroom

--- 06_Frontend/Get_a_Room/src/app/add-new-hotel-roomproperty1/add-new-hotel-roomproperty1.component.ts
+++ 06_Frontend/Get_a_Room/src/app/add-new-hotel-roomproperty1/add-new-hotel-roomproperty1.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AddNewHotelRoomproperty2Component } from '../add-new-hotel-roomproperty2/add-new-hotel-roomproperty2.component';
-
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-new-hotel-roomproperty1',
   standalone: true,
@@ -9,5 +10,20 @@ import { AddNewHotelRoomproperty2Component } from '../add-new-hotel-roomproperty
   styleUrl: './add-new-hotel-roomproperty1.component.css'
 })
 export class AddNewHotelRoomproperty1Component {
+
+  receivedData: any;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      if (params['data']) {
+        const hotelDesc = JSON.parse(decodeURIComponent(params['data']));
+        console.log(hotelDesc);
+        // Use hotelDesc as needed
+      }
+    });
+
+  }
 
 }
